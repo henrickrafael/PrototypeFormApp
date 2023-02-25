da7cd4708230d155a35dfc35a3f886cc3ec5744c
Rafael H <nevesrafel@hotmail.com>
2023-02-25 18:36:20 -0300

    Add project files.

---
+++ frmMain.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SelectFile = New System.Windows.Forms.Button()
+        Me.SecondFile = New System.Windows.Forms.Button()
+        Me.BtnRegerar = New System.Windows.Forms.Button()
+        Me.NumericInput = New System.Windows.Forms.NumericUpDown()
+        Me.LabelInputNumber = New System.Windows.Forms.Label()
+        Me.LinkLabelOutput = New System.Windows.Forms.LinkLabel()
+        CType(Me.NumericInput, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SelectFile
+        '
+        Me.SelectFile.Font = New System.Drawing.Font("Segoe UI", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.SelectFile.Location = New System.Drawing.Point(48, 44)
+        Me.SelectFile.Name = "SelectFile"
+        Me.SelectFile.Size = New System.Drawing.Size(114, 22)
+        Me.SelectFile.TabIndex = 0
+        Me.SelectFile.Text = "Primeiro arquivo"
+        Me.SelectFile.UseVisualStyleBackColor = True
+        '
+        'SecondFile
+        '
+        Me.SecondFile.Font = New System.Drawing.Font("Segoe UI", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.SecondFile.Location = New System.Drawing.Point(48, 82)
+        Me.SecondFile.Name = "SecondFile"
+        Me.SecondFile.Size = New System.Drawing.Size(114, 23)
+        Me.SecondFile.TabIndex = 1
+        Me.SecondFile.Text = "Segundo arquivo"
+        Me.SecondFile.UseVisualStyleBackColor = True
+        '
+        'BtnRegerar
+        '
+        Me.BtnRegerar.Font = New System.Drawing.Font("Segoe UI", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnRegerar.Location = New System.Drawing.Point(48, 230)
+        Me.BtnRegerar.Name = "BtnRegerar"
+        Me.BtnRegerar.Size = New System.Drawing.Size(103, 23)
+        Me.BtnRegerar.TabIndex = 2
+        Me.BtnRegerar.Text = "Regerar"
+        Me.BtnRegerar.UseVisualStyleBackColor = True
+        '
+        'NumericInput
+        '
+        Me.NumericInput.Font = New System.Drawing.Font("Segoe UI", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.NumericInput.Location = New System.Drawing.Point(48, 154)
+        Me.NumericInput.Maximum = New Decimal(New Integer() {1215752192, 23, 0, 0})
+        Me.NumericInput.Name = "NumericInput"
+        Me.NumericInput.Size = New System.Drawing.Size(114, 22)
+        Me.NumericInput.TabIndex = 3
+        '
+        'LabelInputNumber
+        '
+        Me.LabelInputNumber.AutoSize = True
+        Me.LabelInputNumber.Font = New System.Drawing.Font("Segoe UI", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.LabelInputNumber.Location = New System.Drawing.Point(48, 136)
+        Me.LabelInputNumber.Name = "LabelInputNumber"
+        Me.LabelInputNumber.Size = New System.Drawing.Size(83, 13)
+        Me.LabelInputNumber.TabIndex = 4
+        Me.LabelInputNumber.Text = "Valor desejado"
+        '
+        'LinkLabelOutput
+        '
+        Me.LinkLabelOutput.AutoSize = True
+        Me.LinkLabelOutput.Font = New System.Drawing.Font("Segoe UI", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.LinkLabelOutput.Location = New System.Drawing.Point(48, 282)
+        Me.LinkLabelOutput.Name = "LinkLabelOutput"
+        Me.LinkLabelOutput.Size = New System.Drawing.Size(114, 13)
+        Me.LinkLabelOutput.TabIndex = 5
+        Me.LinkLabelOutput.TabStop = True
+        Me.LinkLabelOutput.Text = "Alterar local de saída"
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(506, 317)
+        Me.Controls.Add(Me.LinkLabelOutput)
+        Me.Controls.Add(Me.LabelInputNumber)
+        Me.Controls.Add(Me.NumericInput)
+        Me.Controls.Add(Me.BtnRegerar)
+        Me.Controls.Add(Me.SecondFile)
+        Me.Controls.Add(Me.SelectFile)
+        Me.Name = "FrmMain"
+        Me.Text = "Master"
+        CType(Me.NumericInput, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SelectFile As Button
+    Friend WithEvents SecondFile As Button
+    Friend WithEvents BtnRegerar As Button
+    Friend WithEvents NumericInput As NumericUpDown
+    Friend WithEvents LabelInputNumber As Label
+    Friend WithEvents LinkLabelOutput As LinkLabel
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,130 @@
+﻿Imports System.IO
+
+Public Class FrmMain
+    Private ReadOnly firstFileContent As New List(Of String)
+    Private ReadOnly secondFileContent As New List(Of String)
+    Private ReadOnly maxLenghtValue As Integer = 9
+    Private pathFile As String = "D:\"
+
+    Private Shared Function GetFileContentToList(fileContent As List(Of String)) As Task
+        Try
+            Dim ofd As New OpenFileDialog With {
+                .Filter = "Text files (*.txt) | *.txt"
+            }
+
+            ofd.ShowDialog()
+
+            Using sr As New StreamReader(ofd.FileName)
+
+                While Not sr.EndOfStream
+                    fileContent.Add(sr.ReadLine.Trim())
+                End While
+
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString(), "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        Return Task.CompletedTask
+    End Function
+
+    Private Async Sub SelectFile_Click(sender As Object, e As EventArgs) Handles SelectFile.Click
+        Await GetFileContentToList(firstFileContent)
+    End Sub
+
+    Private Async Sub SecondFile_ClickAsync(sender As Object, e As EventArgs) Handles SecondFile.Click
+        Await GetFileContentToList(secondFileContent)
+    End Sub
+
+    Private Sub ClearGlobals()
+        firstFileContent.Clear()
+        secondFileContent.Clear()
+        pathFile = "D:\"
+    End Sub
+
+    Private Shared Function ReturnFullPathFileName(path As String) As String
+        Return IO.Path.Combine(path, String.Concat(Now.ToString("yyyyMMddHHmmss"), ".txt"))
+    End Function
+
+    Private Sub BtnRegerar_Click(sender As Object, E As EventArgs) Handles BtnRegerar.Click
+        pathFile = ReturnFullPathFileName(pathFile)
+
+        Dim result As New List(Of String)
+        Dim lineIndex As Integer
+
+        Try
+
+            If Not firstFileContent.Any OrElse Not secondFileContent.Any Then
+                MessageBox.Show("Um dos arquivos não foi selecionado!", "Erro",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                ClearGlobals()
+                Return
+            End If
+
+            If NumericInput.Value.ToString.Length > maxLenghtValue Then
+                MessageBox.Show($"O valor digitado ultrapassa o limite permitido de {maxLenghtValue} caracteres!", "Erro",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                ClearGlobals()
+                Return
+            End If
+
+            For Each item In secondFileContent
+
+                For Each linha In firstFileContent
+
+                    If linha.Contains(item) Then
+                        lineIndex += 1
+
+                        linha = Replace(linha, linha.Substring(3, 3), String.Format("{0:000}", lineIndex))
+                        linha = Replace(linha, linha.Substring(25, 9), String.Format("{0:000000000}", NumericInput.Value))
+
+                        result.Add(linha)
+                    End If
+
+                Next
+
+            Next
+
+            If Not File.Exists(pathFile) Then
+                Using fileStream As FileStream = File.Create(pathFile)
+                End Using
+            End If
+
+            Using sw As New StreamWriter(pathFile)
+
+                For Each resultLine In result
+                    sw.WriteLine(resultLine)
+                Next
+
+                result.Clear()
+            End Using
+
+            MessageBox.Show("Arquivo gerado com sucesso!", "Processo concluído", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString(), "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        ClearGlobals()
+    End Sub
+
+    Private Sub LinkLabelOutput_Click(sender As Object, e As EventArgs) Handles LinkLabelOutput.Click
+        Try
+            Using fbd As New FolderBrowserDialog
+                Dim result As DialogResult = fbd.ShowDialog()
+
+                If result = DialogResult.OK Then
+                    pathFile = fbd.SelectedPath
+                End If
+
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString(), "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+End Class
